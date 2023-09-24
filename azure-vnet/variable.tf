6e859074f84b734f24fa9b94e7ba74251c661ec7
Polagoni Anjaneyulu <mailme8anjip@gmail.com>
2023-09-24 21:15:02 +0530

    added VNET files for azure Vnet

---
+++ azure-vnet/variable.tf
@@ -0,0 +1,24 @@
+variable "location" {
+    default = "eastus"
+  
+}
+
+variable "vnetsg" {
+    default = "testsg"
+
+}
+
+variable "vnetname" {
+    default = "mytestvnet"
+  
+}
+
+variable "subnet1" {
+    default = "vnetsub1"
+  
+}
+
+variable "env" {
+    default = "PROD"
+  
+}
---
+++ azure-vnet/vnet.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "rgname" {
+    name = "random_pet.name-anji"
+    location = var.location
+}
+
+resource "random_pet" "name" {}
+
+resource "azurerm_network_security_group" "vnetsg" {
+    name = var.vnetsg
+    location = var.location
+    resource_group_name = azurerm_resource_group.rgname.name
+  
+}
+
+resource "azurerm_virtual_network" "vnetname" {
+    name = var.vnetname
+    location = var.location
+    resource_group_name = azurerm_resource_group.rgname.name
+    address_space = ["10.0.0.0/16"]
+  
+}
+
+subnet {
+    name = var.subnet1
+    address_prifix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.vnetsg.id
+}
+
+tags = {
+    env = var.env
+}
+
